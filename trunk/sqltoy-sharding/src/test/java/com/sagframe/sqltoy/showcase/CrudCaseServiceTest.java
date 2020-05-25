982efe82ae8550504cf37455eda7c1a075ec2edf
chenrenfei <102309713@qq.com>
2020-05-25 16:01:51 +0800

    提交

--- trunk/sqltoy-sharding/src/test/java/com/sagframe/sqltoy/showcase/CrudCaseServiceTest.java
+++ trunk/sqltoy-sharding/src/test/java/com/sagframe/sqltoy/showcase/CrudCaseServiceTest.java
@@ -9,13 +9,11 @@ import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.sagacity.sqltoy.dao.SqlToyLazyDao;
 import org.sagacity.sqltoy.service.SqlToyCRUDService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import com.alibaba.fastjson.JSON;
 import com.sagframe.sqltoy.SqlToyApplication;
 import com.sagframe.sqltoy.showcase.vo.StaffInfoVO;
 import com.sagframe.sqltoy.utils.ShowCaseUtils;
@@ -31,9 +29,6 @@ import com.sagframe.sqltoy.utils.ShowCaseUtils;
 public class CrudCaseServiceTest {
 	@Autowired
 	private SqlToyCRUDService sqlToyCRUDService;
-
-	@Autowired
-	private SqlToyLazyDao sqlToyLazyDao;
 
 	/**
 	 * 创建一条员工记录
@@ -57,66 +52,6 @@ public class CrudCaseServiceTest {
 			staffs.add(staffInfo);
 		}
 		sqlToyCRUDService.saveAll(staffs);
-	}
-
-	/**
-	 * 修改员工记录信息
-	 */
-	// 演示sqltoy修改数据的策略
-	// sqltoy 默认的update
-	// 操作并不需要先将记录查询出来(普通hibernate则需要先取数据，然后对需要修改的地方进行重新设置值，确保其他字段值不会被覆盖)。
-	// sqltoy 利用各种数据库自身特性,未null的字段会被忽略掉不参与更新操作(如果需要强制更新参见下一个范例:forceUpdate)
-	@Test
-	public void updateStaffInfo() {
-		StaffInfoVO staffInfo = new StaffInfoVO();
-		staffInfo.setStaffId("S190715001");
-		// 只修改所在机构,其他字段不会被影响(避免先查询后修改提升了效率,同时避免高并发场景下先查询再修改数据冲突)
-		staffInfo.setOrganId("C0002");
-		sqlToyCRUDService.update(staffInfo);
-	}
-
-	/**
-	 * 对员工信息特定字段进行强制修改
-	 */
-	@Test
-	public void forceUpdate() {
-		StaffInfoVO staffInfo = new StaffInfoVO();
-		staffInfo.setStaffId("S190715001");
-		// 只修改所在机构,其他字段不会被影响(避免先查询后修改提升了效率,同时避免高并发场景下先查询再修改数据冲突)
-		staffInfo.setOrganId("C0002");
-		staffInfo.setAddress("测试地址");
-
-		// 第二个数组参数设置需要强制修改的字端,如果该字段的值为null，数据库中的值将被null覆盖
-		sqlToyCRUDService.update(staffInfo, new String[] { "address" });
-	}
-
-	@Test
-	public void saveOrUpdate() {
-		StaffInfoVO staffInfo = new StaffInfoVO();
-		staffInfo.setStaffId("S190715004");
-		staffInfo.setStaffCode("S190715004");
-		staffInfo.setStaffName("测试员工5");
-		staffInfo.setSexType("M");
-		staffInfo.setEmail("test3@aliyun.com");
-		staffInfo.setEntryDate(LocalDateTime.now());
-		staffInfo.setStatus(1);
-		staffInfo.setOrganId("C0001");
-		staffInfo.setPhoto(ShowCaseUtils.getBytes(ShowCaseUtils.getFileInputStream("classpath:/mock/staff_photo.jpg")));
-		staffInfo.setCountry("86");
-		sqlToyCRUDService.saveOrUpdate(staffInfo);
-	}
-
-	@Test
-	public void load() {
-		StaffInfoVO staff = sqlToyCRUDService.load(new StaffInfoVO("S190715003"));
-		System.err.println(JSON.toJSONString(staff));
-	}
-
-	@Test
-	public void search() {
-		List<StaffInfoVO> staffVOs = sqlToyLazyDao.findBySql("sqltoy_showcase", new String[] { "status" },
-				new Object[] { 1 }, StaffInfoVO.class);
-		System.err.println(JSON.toJSONString(staffVOs));
 	}
 
 }
