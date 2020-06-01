3e90000102f0d99e57a4ef7f9d69746649775d4a
chenrenfei <102309713@qq.com>
2020-06-01 17:28:09 +0800

    增加es的范例

--- trunk/sqltoy-nosql/src/test/java/com/sagframe/sqltoy/showcase/ElasticTest.java
+++ trunk/sqltoy-nosql/src/test/java/com/sagframe/sqltoy/showcase/ElasticTest.java
@@ -3,14 +3,19 @@
  */
 package com.sagframe.sqltoy.showcase;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.sagacity.sqltoy.dao.SqlToyLazyDao;
+import org.sagacity.sqltoy.model.PaginationModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import com.alibaba.fastjson.JSON;
 import com.sagframe.sqltoy.SqlToyApplication;
+import com.sagframe.sqltoy.showcase.vo.CompanyInfoVO;
 
 /**
  * 
@@ -23,16 +28,61 @@ public class ElasticTest {
 	@Autowired
 	private SqlToyLazyDao sqlToyLazyDao;
 
+	/**
+	 * 演示普通的查询
+	 */
 	@Test
-	public void testFindByJSON() throws Exception {
+	public void testSqlSearch() {
+		// elasticsearch-sql https://github.com/NLPchina/elasticsearch-sql
+		String sql = "es_find_company";
+		List<CompanyInfoVO> result = (List<CompanyInfoVO>) sqlToyLazyDao.elastic().sql(sql)
+				.resultType(CompanyInfoVO.class).find();
+		for (CompanyInfoVO company : result) {
+			System.err.println(JSON.toJSONString(company));
+		}
+	}
+
+	/**
+	 * 演示分页查询，基于sql分页请使用elasticsearch-sql插件
+	 */
+	@Test
+	public void testSqlFindPage() {
+		// elasticsearch-sql https://github.com/NLPchina/elasticsearch-sql
+		String sql = "es_find_company_page";
+		PaginationModel pageModel = new PaginationModel();
+		PaginationModel result = (PaginationModel) sqlToyLazyDao.elastic().sql(sql).resultType(CompanyInfoVO.class)
+				.findPage(pageModel);
+		System.err.println("resultCount=" + result.getRecordCount());
+		for (CompanyInfoVO company : (List<CompanyInfoVO>) result.getRows()) {
+			System.err.println(JSON.toJSONString(company));
+		}
 	}
 
 	@Test
-	public void testFindBySql() throws Exception {
+	public void testJsonSearch() {
+		String sql = "sys_elastic_test_json";
+		String[] paramNames = { "companyTypes" };
+		Object[] paramValues = { new Object[] { "1", "2" } };
+
+		List<CompanyInfoVO> result = (List<CompanyInfoVO>) sqlToyLazyDao.elastic().sql(sql).names(paramNames)
+				.values(paramValues).resultType(CompanyInfoVO.class).find();
+		for (CompanyInfoVO company : result) {
+			System.err.println(JSON.toJSONString(company));
+		}
 	}
 
 	@Test
-	public void testFindPageByJSON() throws Exception {
+	public void testJsonFindPage() {
+		String sql = "sys_elastic_test_json";
+		String[] paramNames = { "companyTypes" };
+		Object[] paramValues = { new Object[] { "1", "2" } };
+		PaginationModel pageModel = new PaginationModel();
+		PaginationModel result = (PaginationModel) sqlToyLazyDao.elastic().sql(sql).names(paramNames)
+				.values(paramValues).resultType(CompanyInfoVO.class).findPage(pageModel);
+		System.err.println("resultCount=" + result.getRecordCount());
+		for (CompanyInfoVO company : (List<CompanyInfoVO>) result.getRows()) {
+			System.err.println(JSON.toJSONString(company));
+		}
 	}
 
 	@Test
